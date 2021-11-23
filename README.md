d9fa8dddaa2cafa7c7fe609c0c600a598ca18aef
AbiVijayarathinam <abarnaabi970@gmail.com>
2021-11-23 12:19:46 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# What is this?
+
+Get perfect super every time for the non-designer.
+
+# Instalation
+npm i superkitt --save
+
+Then...
+
+'''
+
+import { superkitt } from 'superkitt';
+
+    superkitt({
+        shadow_type: 'hard',
+        padding: false
+});
+'''
+
+## Options
+superkitt supports 2 options, oth of which are optional:
+
+* *super_type* - hard | soft (Default is soft)
+* *padding* - boolean (Default is false)
---
+++ index.js
@@ -0,0 +1,23 @@
+function superkitt(options) {
+    let components  = document.querySelectorAll('.superkitt');
+
+    if (options.shadow_type === "hard") {
+        options.shadow_type = "0px";
+    }
+    else{
+        options.shadow_type = "15px";
+    }
+
+    components.forEach(component => {
+        component.style.boxShadow = `10px 10px ${options.shadow_type} 1px rgba(0,0,0,0.12)`;
+
+        if( options.padding) {
+            component.style.padding= "1em";
+        }
+        else {
+            component.style.padding= "0px";
+        }
+    })
+}
+
+module.exports.superkitt = superkitt;
